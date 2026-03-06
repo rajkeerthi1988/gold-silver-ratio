0274a16d20bf9feb9322b2c59368a44e739fa060
rajkeerthi1988 <60472471+rajkeerthi1988@users.noreply.github.com>
2026-03-06 21:57:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 async function getPrices(){
 
+try{
+
 let goldResponse = await fetch("https://api.gold-api.com/price/XAU")
 let goldData = await goldResponse.json()
 
@@ -14,9 +16,30 @@ document.getElementById("silver").innerHTML = "$" + silver
 
 let ratio = gold / silver
 
-document.getElementById("ratio").innerHTML =
-ratio.toFixed(2)
+document.getElementById("ratio").innerHTML = ratio.toFixed(2)
+
+let signal = ""
+
+if(ratio > 80){
+signal = "Silver may be undervalued"
+}
+else if(ratio < 50){
+signal = "Gold may be undervalued"
+}
+else{
+signal = "Ratio in normal range"
+}
+
+document.getElementById("signal").innerHTML = signal
+
+}catch(error){
+
+console.log("Price fetch failed")
+
+}
 
 }
 
 getPrices()
+
+setInterval(getPrices,60000)
