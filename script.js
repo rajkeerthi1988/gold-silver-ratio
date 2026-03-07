3f8dc8b5d60afb8e43ae49b5d5e51102e245f551
rajkeerthi1988 <60472471+rajkeerthi1988@users.noreply.github.com>
2026-03-07 18:32:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,26 +11,14 @@ let silverData = await silverResponse.json()
 let gold = goldData.price
 let silver = silverData.price
 
-document.getElementById("gold").innerHTML = "$" + gold
-document.getElementById("silver").innerHTML = "$" + silver
+document.getElementById("gold").innerHTML = "$" + gold.toFixed(2)
+document.getElementById("silver").innerHTML = "$" + silver.toFixed(2)
 
 let ratio = gold / silver
 
 document.getElementById("ratio").innerHTML = ratio.toFixed(2)
 
-let signal = ""
-
-if(ratio > 80){
-signal = "Silver may be undervalued"
-}
-else if(ratio < 50){
-signal = "Gold may be undervalued"
-}
-else{
-signal = "Ratio in normal range"
-}
-
-document.getElementById("signal").innerHTML = signal
+updateChart(ratio)
 
 }catch(error){
 
@@ -43,3 +31,36 @@ console.log("Price fetch failed")
 getPrices()
 
 setInterval(getPrices,60000)
+
+let labels = []
+let dataPoints = []
+
+const ctx = document.getElementById("ratioChart")
+
+const ratioChart = new Chart(ctx,{
+type:'line',
+data:{
+labels:labels,
+datasets:[{
+label:"Gold Silver Ratio",
+data:dataPoints,
+borderWidth:2
+}]
+}
+})
+
+function updateChart(ratio){
+
+let time = new Date().toLocaleTimeString()
+
+labels.push(time)
+dataPoints.push(ratio)
+
+if(labels.length > 20){
+labels.shift()
+dataPoints.shift()
+}
+
+ratioChart.update()
+
+}
