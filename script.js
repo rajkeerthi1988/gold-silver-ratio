ce2a8f588b1683aa8f01987f89e27c4a2847dfa0
rajkeerthi1988 <60472471+rajkeerthi1988@users.noreply.github.com>
2026-03-07 20:14:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -67,5 +67,24 @@ ratioChart.update()
 
 getPrices()
 
-
 setInterval(getPrices,5000)
+
+<script src="https://s3.tradingview.com/tv.js"></script>
+
+<script>
+new TradingView.widget({
+  "container_id": "tradingview_ratio",
+  "width": "100%",
+  "height": 500,
+  "symbol": "TVC:GOLD/TVC:SILVER",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": false
+});
+</script>
+
